3c63f7b8c1bb480e28b5ac34b40d2bb08f6ce3f0
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2022-01-08 15:47:16 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FINALNE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FINALNE.dir/main.cpp.obj"
+  "FINALNE.exe"
+  "FINALNE.exe.manifest"
+  "FINALNE.pdb"
+  "libFINALNE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FINALNE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
